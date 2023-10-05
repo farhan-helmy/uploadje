ad22ba0a7ded7e78fd6ac729dcb50e55534fbea3
Khairul Haaziq <rhycoz@yahoo.com>
2023-10-06 05:40:47 +0800

    fix: use env for query pg connection string

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,9 +3,11 @@ import postgres from "postgres"
 import * as users from "./schema/users"
 import * as apps from "./schema/apps"
 import * as images from "./schema/images"
+import dotenv from 'dotenv';
 
+dotenv.config();
 
-const queryClient = postgres("postgres://postgres:mysecretpassword@localhost:5432/uploadjedev");
+const queryClient = postgres(process.env.DB_URL!);
 const db = drizzle(queryClient, {
     schema: {
         ...users,
