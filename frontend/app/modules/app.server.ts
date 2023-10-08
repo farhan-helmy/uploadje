2a6a90fb132e54226b3b91551b5f087f717c7542
Khairul Haaziq <rhycoz@yahoo.com>
2023-10-08 14:39:35 +0800

    feat(frontend): make name optional for creating apps

--- frontend/app/modules/app.server.ts
+++ frontend/app/modules/app.server.ts
@@ -54,7 +54,7 @@ class App {
         headers: {
           'Authorization': `Bearer ${token}`
         },
-        body: {
+        body: name && {
           name
         },
         parseResponse: JSON.parse
